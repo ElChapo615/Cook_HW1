e07ec180fe99f0fef1f75e682e22c7cfbe344629
ElChapo615 <poorlittletinktink@gmail.com>
2025-03-04 11:43:38 +0000

    Add Data to DB

--- kmdb.sql
+++ kmdb.sql
@@ -12,10 +12,10 @@ DROP TABLE IF EXISTS movie;
 CREATE TABLE movies (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   title TEXT,
-  studio TEXT
   release_year TEXT,
-  rating TEXT
-  director TEXT
+  rating TEXT,
+  studio TEXT,
+  director TEXT,
   producer TEXT
 );
 
@@ -24,14 +24,44 @@ CREATE TABLE actor (
   first_name TEXT,
   last_name TEXT,
   movie_role TEXT,
-  title TEXT
+  title TEXT,
   movies INTEGER
 );
 
 CREATE TABLE studio (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   movies INTEGER,
-  title TEXT
+  title TEXT,
   release_year TEXT
 );
   
+  INSERT INTO movies (
+  title,
+  release_year,
+  rating,
+  studio
+  )
+
+  VALUES (
+    "Batman Begins",
+    "2005",
+    "PG-13",
+    "Warner Bros"
+
+  );
+
+
+
+-- Batman Begins          2005           PG-13  Warner Bros.
+-- The Dark Knight        2008           PG-13  Warner Bros.
+-- The Dark Knight Rises  2012           PG-13  Warner Bros.
+
+--VALUES 
+--  "Jane",
+ -- "Doe",
+ -- #"jane@example.com"
+--);
+
+--We can then SELECT * FROM students; to see if it worked:
+
+SELECT * FROM Movies
